ebc45539f2a4f3e3fe4b7913bc6a0d221d553966
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-08-29 10:18:49 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
+const dotenv = require("dotenv")
+dotenv.config({ path: "config.env" })
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
+// const io = require("socket.io")(3001, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// })
+const express = require("express")
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server, {
+    cors: {
+      methods: ["GET", "POST"],
+    }
+    })
+
+const defaultValue = ""
+
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
+const PORT = process.env.PORT || 3001
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+}
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
